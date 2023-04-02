e39690c8ff66a7cd66db82693c2c527ab4bbdb43
PuckVoorham <puckvoorham@gmail.com>
2023-04-02 23:47:26 +0200

    adding description first

--- extra stuff.R
+++ extra stuff.R
@@ -7,3 +7,20 @@
 #   #plot.background = element_rect(fill = "lightblue"),
 #   plot.margin = unit(c(5, 10, 5, 10), units = "mm")
 # )
+
+
+
+# scatter_plot <- ggplot(Categorical, aes(x = valence, y = energy, size = tempo, color = playlist_name)) +
+#   geom_point() +
+#   labs(
+#     x = "Valence",
+#     y = "Energy"
+#   ) + theme_minimal() +
+#   theme(
+#     text = element_text( color = "black"),
+#     plot.subtitle = element_text(size = 12, color = "black"),
+#     #plot.background = element_rect(fill = "lightblue"),
+#     plot.margin = unit(c(5, 10, 5, 10), units = "mm")
+#   )
+#
+# scatter_plot
