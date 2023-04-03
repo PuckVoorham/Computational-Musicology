ead0523666b465b24e9b098efd7eaf08b9530b53
PuckVoorham <puckvoorham@gmail.com>
2023-04-03 03:23:17 +0200

    lots of changes

--- make-SSM.R
+++ make-SSM.R
@@ -5,7 +5,90 @@ library(compmus)
 library(ggplot2)
 
 
-SSM <-
+SSM_1 <-
+  get_tidy_audio_analysis("2wxeIcUPu0hdlB8pFsxSAi") |> # Change URI.
+  compmus_align(bars, segments) |>                     # Change `bars`
+  select(bars) |>                                      #   in all three
+  unnest(bars) |>                                      #   of these lines.
+  mutate(
+    pitches =
+      map(segments,
+          compmus_summarise, pitches,
+          method = "rms", norm = "euclidean"              # Change summary & norm.
+      )
+  ) |>
+  mutate(
+    timbre =
+      map(segments,
+          compmus_summarise, timbre,
+          method = "rms", norm = "euclidean"              # Change summary & norm.
+      )
+  )
+
+SSM_1 |>
+  compmus_self_similarity(timbre, "cosine") |>
+  ggplot(
+    aes(
+      x = xstart + xduration / 2,
+      width = xduration,
+      y = ystart + yduration / 2,
+      height = yduration,
+      fill = d
+    )
+  ) +
+  geom_tile() +
+  coord_fixed() +
+  scale_fill_viridis_c(guide = "none") +
+  theme_classic() +
+  labs(x = "", y = "")
+
+saveRDS(object = SSM_1, file = "data/SSM_1 (SSM).RDS")
+
+
+
+
+SSM_2 <-
+  get_tidy_audio_analysis("6K4t31amVTZDgR3sKmwUJJ") |> # Change URI.
+  compmus_align(bars, segments) |>                     # Change `bars`
+  select(bars) |>                                      #   in all three
+  unnest(bars) |>                                      #   of these lines.
+  mutate(
+    pitches =
+      map(segments,
+          compmus_summarise, pitches,
+          method = "rms", norm = "euclidean"              # Change summary & norm.
+      )
+  ) |>
+  mutate(
+    timbre =
+      map(segments,
+          compmus_summarise, timbre,
+          method = "rms", norm = "euclidean"              # Change summary & norm.
+      )
+  )
+
+SSM_2 |>
+  compmus_self_similarity(timbre, "cosine") |>
+  ggplot(
+    aes(
+      x = xstart + xduration / 2,
+      width = xduration,
+      y = ystart + yduration / 2,
+      height = yduration,
+      fill = d
+    )
+  ) +
+  geom_tile() +
+  coord_fixed() +
+  scale_fill_viridis_c(guide = "none") +
+  theme_classic() +
+  labs(x = "", y = "")
+
+saveRDS(object = SSM_2, file = "data/SSM_2 (SSM).RDS")
+
+
+
+SSM_3 <-
   get_tidy_audio_analysis("1KcOU0q9AMXV9xi4tSzzCc") |> # Change URI.
   compmus_align(bars, segments) |>                     # Change `bars`
   select(bars) |>                                      #   in all three
@@ -25,7 +108,7 @@ SSM <-
       )
   )
 
-SSM |>
+SSM_3 |>
   compmus_self_similarity(timbre, "cosine") |>
   ggplot(
     aes(
@@ -42,5 +125,5 @@ SSM |>
   theme_classic() +
   labs(x = "", y = "")
 
-saveRDS(object = SSM, file = "data/SSM (SSM).RDS")
+saveRDS(object = SSM_3, file = "data/SSM_3 (SSM).RDS")
 
